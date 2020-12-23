c45f3d7a6d84b8d755ac78b578874d4b0ff7a4e0
Tyler Ruggles <tylerhruggles@gmail.com>
2020-12-23 14:00:09 -0500

    Adding 5 years and merging

--- plotting-scripts/merge_years.py
+++ plotting-scripts/merge_years.py
@@ -13,9 +13,9 @@ years = [2016, 2017, 2018, 2019, 2020]
 
 def get_output_files(f_base):
 
-    print(f_base+'*.csv')
+    #print(f_base+'*.csv')
     files = glob(f_base+'*.csv')
-    print(files)
+    #print(files)
     assert(len(files) == 1)
     return files[0]
 
@@ -26,27 +26,43 @@ def merge_files(scenario, f_names):
         if i == 0:
             master = pd.read_csv(f, index_col='case name')
             master = master.drop(columns=['problem status',])
-            print("Master")
-            print(master.head())
+            #print("Master")
+            #print(master.head())
         else:
             df = pd.read_csv(f, index_col='case name')
             df = df.drop(columns=['problem status',])
-            print("New DF")
-            print(df.head())
+            #print("New DF")
+            #print(df.head())
             master = master + df
-            print("Updated Master")
-            print(master.head())
+            #print("Updated Master")
+            #print(master.head())
 
     master = master / float(len(f_names)) # float to make sure there's no Int rounding
 
-    print(master.head())
+    #print(master.head())
+    print(master.shape)
 
     master.to_csv(f'../Output_Data/aggregates/{scenario}_2015-2020.csv')
     return master
 
 
 scenarios = [
-        #"8-input_unmet_demand-wind",
+        "1-input_unmet_demand_constant-cp0",
+        "1-input_unmet_demand-cp0",
+        "2-input_unmet_demand_constant-cp200",
+        "2-input_unmet_demand-cp200",
+        "3-input_unmet_demand-constant-wind-solar-storage",
+        "3-input_unmet_demand-wind-solar-storage",
+        "4-input_unmet_demand-constant-wind-storage",
+        "4-input_unmet_demand-wind-storage",
+        "5-input_unmet_demand-constant-solar-storage",
+        "5-input_unmet_demand-solar-storage",
+        "6-input_unmet_demand-constant-wind-solar",
+        "6-input_unmet_demand-wind-solar",
+        "7-input_unmet_demand-constant-solar",
+        "7-input_unmet_demand-solar",
+        "8-input_unmet_demand-constant-wind",
+        "8-input_unmet_demand-wind",
         ]
 
 for storage in ["0.01", "0.1", "1", "10", "100"]:
